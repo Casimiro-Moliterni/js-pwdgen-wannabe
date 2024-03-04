55f754c9ba26bb514ca63318899ff8bc0ed85b0e
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-03-04 16:52:33 +0100

    add: number js

--- js/script.js
+++ js/script.js
@@ -11,10 +11,21 @@ const UserSecondName = prompt('Dimmi il tuo cognome');
 // prendo il colore prefrito dall'utente
 const UserColorFavourit = prompt('Dimmi il tuo colore preferito');
 
+// prendo il numero dall'utente
+const UserNumber = prompt('Dimmi un numero');
+
+const userAgeAsNumber = parseInt(UserNumber);
+
+
 // numero di default 
 
-const NumberDefault=23;
+const NumberDefault = 23 ;
+
+// Logica della mia applicazione 
+
+const NumberPass = userAgeAsNumber + NumberDefault ;
+console.log(NumberPass)
 
 // password generata 
-const UserMessage = `${UserName}${UserSecondName}${UserColorFavourit}${NumberDefault}`;
+const UserMessage = `${UserName}${UserSecondName}${UserColorFavourit}${NumberPass}`;
 document.getElementById('passwordtitle').innerHTML = UserMessage;
